93614013be90133481f484549098c910ce37e406
Jonathon Hill <jonathon@compwright.com>
2025-03-31 14:28:39 -0400

    feat: add additional getters to DomoResourceOwner

--- src/DomoResourceOwner.php
+++ src/DomoResourceOwner.php
@@ -2,63 +2,79 @@
 
 namespace Compwright\OAuth2\Domo;
 
-use Countable;
 use JsonSerializable;
-use League\OAuth2\Client\Provider\ResourceOwnerInterface;
+use League\OAuth2\Client\Provider\GenericResourceOwner;
 
-class DomoResourceOwner implements ResourceOwnerInterface, Countable, JsonSerializable
+class DomoResourceOwner extends GenericResourceOwner implements JsonSerializable
 {
-    /**
-     * @param array<string, int|string|null> $response
-     */
-    public function __construct(protected array $response = [])
-    {
-    }
-
-    public function count(): int
-    {
-        return count($this->response);
-    }
-
     /**
      * @return array<string, int|string|null>
      */
     public function jsonSerialize(): array
     {
-        return $this->response;
+        return $this->toArray();
     }
 
-    /**
-     * Get resource owner id
-     */
-    public function getId(): string
+    public function getTitle(): string
     {
-        return strval($this->response['id'] ?? '');
+        return strval($this->response['title'] ?? '');
     }
 
-    /**
-     * Get resource owner email
-     */
     public function getEmail(): string
     {
         return strval($this->response['email'] ?? '');
     }
 
-    /**
-     * Get resource owner name
-     */
+    public function getRole(): string
+    {
+        return strval($this->response['role'] ?? '');
+    }
+
+    public function getPhone(): string
+    {
+        return strval($this->response['phone'] ?? '');
+    }
+
     public function getName(): string
     {
         return strval($this->response['name'] ?? '');
     }
 
-    /**
-     * Return all of the owner details available as an array.
-     *
-     * @return array<string, int|string|null>
-     */
-    public function toArray(): array
+    public function getDepartment(): string
     {
-        return $this->jsonSerialize();
+        return strval($this->response['department'] ?? '');
+    }
+
+    public function getRoleId(): int
+    {
+        return intval($this->response['roleId'] ?? '');
+    }
+
+    public function getCreatedAt(): string
+    {
+        return strval($this->response['createdAt'] ?? '');
+    }
+
+    public function getUpdatedAt(): string
+    {
+        return strval($this->response['updatedAt'] ?? '');
+    }
+
+    public function getDeleted(): bool
+    {
+        return boolval($this->response['deleted'] ?? '');
+    }
+
+    public function getImage(): string
+    {
+        return strval($this->response['image'] ?? '');
+    }
+
+    /**
+     * @return array<array{id:int, name:string}>
+     */
+    public function getGroups(): array
+    {
+        return (array) ($this->response['groups'] ?? []);
     }
 }
